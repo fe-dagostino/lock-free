432f9f2228c970c7bd951fd1b74b7982dfdd5304
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-07-29 00:55:02 +0200

    empty file as workaround for CMakeFiles.txt

---
+++ src/config.cpp
@@ -0,0 +1,11 @@
+#ifndef LOCK_FREE_CONFIG_H
+#define LOCK_FREE_CONFIG_H
+
+
+#define LIB_VERSION_MAJOR           0
+#define LIB_VERSION_MINOR           1
+#define LIB_VERSION_PATCH           0
+
+#define LIB_VERSION   v0_1_0
+
+#endif //LOCK_FREE_CONFIG_H
