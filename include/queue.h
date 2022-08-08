17c83e9995a02beed55af814066528eb9c055b55
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-08-08 14:46:41 +0200

    node_t<> moved to types.h as generic structure to be specialized.

--- include/queue.h
+++ include/queue.h
@@ -31,6 +31,7 @@
 #include <cstddef>
 
 #include "arena_allocator.h"
+#include "core/types.h"
 
 namespace lock_free {
 
@@ -57,11 +58,9 @@ inline namespace LIB_VERSION {
  *               A value different greater than 0 will have the effect to limit max number of items on 
  *               the single queue.
 */
-
-struct node_t;
-
 template<typename data_t, typename data_size_t,  
-         data_size_t chunk_size = 1024, data_size_t reserve_size = chunk_size, data_size_t max_size = 0>
+         data_size_t chunk_size = 1024, data_size_t reserve_size = chunk_size, data_size_t max_size = 0,
+         typename arena_t = arena_allocator<core::node_t<data_t,false,true,true>, data_size_t, chunk_size, reserve_size, max_size, chunk_size / 3, core::virtual_allocator<data_size_t>> >
 requires std::is_unsigned_v<data_size_t> && (std::is_same_v<data_size_t,u_int32_t> || std::is_same_v<data_size_t,u_int64_t>)
          && ( ((sizeof(data_t) % alignof(std::max_align_t)) == 0 ) || ((sizeof(std::max_align_t) % alignof(data_t)) == 0 ) )
          && (chunk_size >= 1)
@@ -75,50 +74,8 @@ public:
   using const_reference = const data_t&;
   using pointer         = data_t*;
   using const_pointer   = const data_t*;
- 
-private:
-  /**
-   * @brief A single node for the queue with pointer to next item.
-   *        As for design decision each node_t is indipendent and 
-   *        we pay the cost of this decision in memory allocation time.
-   *        On the other side this keep the code readable and estensible.
-   */
-  struct node_t
-  {
-    /**
-     * @brief Default constructor.
-     */
-    constexpr inline node_t() noexcept
-      : _next(nullptr)
-    { }
-
-    /**
-     * @brief Constructor thaht allow to specify rvalue for the node.
-     */
-    constexpr inline node_t( const value_type& value ) noexcept
-      : _next(nullptr), _data(value)
-    { }
-
-    /**
-     * @brief Constructor that allow to specify value for the node.
-     */
-    constexpr inline node_t( value_type&& value ) noexcept
-      : _next(nullptr), _data( std::move(value) )
-    { }
-
-    /**
-     * @brief Destructor.
-     *        Note: deallocation is performed at queue_t level in order to avoid
-     *              stack overflow when deallocating millions of items.
-     */
-    constexpr inline ~node_t() noexcept
-    { }
-
-    std::atomic<node_t*>   _next;
-    value_type             _data;
-  };
-
-  using arena_type      = arena_allocator<node_t, size_type, chunk_size, reserve_size, max_size, chunk_size / 3, core::virtual_allocator<size_type> >;
+  using node_type       = core::node_t<data_t,false,true,true>;
+  using arena_type      = arena_t;
 
 public:
   
@@ -147,20 +104,20 @@ public:
   template<typename value_type>
   constexpr inline bool      push( value_type&& data ) noexcept
   {
-    node_t* new_node = create_node(data);
+    node_type* new_node = create_node(data);
     if ( new_node == nullptr )
       return false;
 
     for (;;)
     {
-      node_t* old_tail = _tail.load( std::memory_order_acquire );
+      node_type* old_tail = _tail.load( std::memory_order_acquire );
 
       if ( old_tail == nullptr )
       {
         if ( _tail.compare_exchange_weak( old_tail, new_node, std::memory_order_release, std::memory_order_acquire ) == false )
           continue;
 
-        node_t* old_head = _head.load( std::memory_order_acquire );
+        node_type* old_head = _head.load( std::memory_order_acquire );
         if ( old_head == nullptr )
         { _head.compare_exchange_weak( old_head, new_node, std::memory_order_release, std::memory_order_relaxed ); }
 
@@ -168,7 +125,7 @@ public:
       }
       else
       {
-        node_t* old_next = old_tail->_next.load( std::memory_order_acquire );
+        node_type* old_next = old_tail->_next.load( std::memory_order_acquire );
 
         if ( old_next == nullptr )     
         {
@@ -177,7 +134,7 @@ public:
 
           _tail.exchange( new_node, std::memory_order_release );
           
-          node_t* old_head = _head.load( std::memory_order_acquire );
+          node_type* old_head = _head.load( std::memory_order_acquire );
           if ( old_head == nullptr )
           { _head.compare_exchange_weak( old_head, new_node, std::memory_order_release, std::memory_order_relaxed ); }
 
@@ -196,7 +153,7 @@ public:
   /***/
   constexpr inline bool      pop( value_type& data ) noexcept
   {
-    node_t* cur_head  = _head.load( std::memory_order_acquire );
+    node_type* cur_head  = _head.load( std::memory_order_acquire );
     if ( cur_head == nullptr )
     { return false; }
 
@@ -206,7 +163,7 @@ public:
     if ( cur_head == nullptr )
     { return false; }
 
-    node_t* cur_tail = _tail.load( std::memory_order_acquire );
+    node_type* cur_tail = _tail.load( std::memory_order_acquire );
     
     if ( cur_head == cur_tail )
     { _tail.compare_exchange_weak( cur_tail, nullptr, std::memory_order_acq_rel, std::memory_order_relaxed ); }
@@ -244,7 +201,7 @@ private:
    * @return constexpr node_t*  return a pointer to the node with 'data' and 'next' initialized. 
    */
   template<typename value_type>
-  constexpr inline node_t* create_node ( value_type& data ) noexcept
+  constexpr inline node_type* create_node ( value_type& data ) noexcept
   { 
     return _arena.allocate(data);
   }
@@ -254,7 +211,7 @@ private:
    * 
    * @param node  pointer to node to be used from create_node()
    */
-  constexpr inline void    destroy_node( node_t* node ) noexcept
+  constexpr inline void       destroy_node( node_type* node ) noexcept
   { 
     _arena.deallocate(node);
   }
@@ -262,11 +219,11 @@ private:
 private:
   arena_type                _arena;
   std::atomic<size_type>    _items;
-  std::atomic<node_t*>      _head;
-  std::atomic<node_t*>      _tail;
+  std::atomic<node_type*>   _head;
+  std::atomic<node_type*>   _tail;
  
   static constexpr const size_type data_type_size = sizeof(value_type);
-  static constexpr const size_type node_type_size = sizeof(node_t);
+  static constexpr const size_type node_type_size = sizeof(node_type);
   
 };
 
