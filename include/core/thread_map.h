9d20662b176f4af06d7d6d8d98dba163fa74f4a5
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-08-19 20:08:31 +0200

    basic implementation for a thread indexer

---
+++ include/core/thread_map.h
@@ -0,0 +1,81 @@
+/**************************************************************************************************
+ * 
+ * Copyright 2022 https://github.com/fe-dagostino
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *************************************************************************************************/
+
+#ifndef CORE_THREAD_MAP_H
+#define CORE_THREAD_MAP_H
+
+#include "types.h"
+#include <unordered_map>
+
+
+namespace core {
+
+inline namespace LIB_VERSION {
+
+/**
+ * @brief Generating an unique ID for each thread instance.
+ * 
+ * @tparam data_size_t  data type to be used for numeric values.
+ * @tparam base         specify if first ID start from 1 (default) or
+ *                      from a a different positive number.
+ */
+template<typename data_size_t, data_size_t base = 1>
+  requires std::is_unsigned_v<data_size_t>          
+struct thread_map final
+{
+public:  
+  using value_type      = std::thread::id;
+  using size_type       = data_size_t;
+  using hash_map        = std::unordered_map<value_type, size_type>;
+
+public:
+  /***/
+  constexpr inline thread_map() noexcept
+    : _counter(base)
+  {}
+  
+  /***/
+  constexpr inline size_type  add( const value_type& tid = std::this_thread::get_id() ) noexcept
+  {
+    if ( _map.contains(tid) )
+      return _map.at(tid);
+    
+    _mtx.lock();
+    _map[tid] = ++_counter;
+    _mtx.unlock();
+
+    return _counter;
+  }
+
+private:
+  std::mutex  _mtx;
+  hash_map    _map;
+
+  size_type   _counter;
+};
+
+} // namespace LIB_VERSION 
+
+}
+
+#endif // CORE_THREAD_MAP_H
