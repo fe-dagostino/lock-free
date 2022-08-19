f6e9eced9eb9f9f2eb85b89357b05bb326c6940f
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-08-19 20:12:48 +0200

    multi_queue using example

--- examples/mqueue.cpp
+++ examples/mqueue.cpp
@@ -51,10 +51,10 @@ int main( int argc, const char* argv[] )
   (void)argc;
   (void)argv;
 
-  const u_int32_t single_thread_repeat = 1000000;
-  const u_int32_t nthreads = 8;
+  const uint32_t single_thread_repeat = 5000000;
+  const uint32_t nthreads = 8;
 
-  lock_free::multi_queue<u_int64_t,u_int32_t,nthreads,1,1000000>  mqueue;
+  lock_free::multi_queue<u_int64_t,uint32_t, nthreads,1024,10000000,0>  mqueue;
   
   std::thread* prod_X[nthreads];
 
@@ -62,18 +62,23 @@ int main( int argc, const char* argv[] )
   // Read START TIME
   auto tp_start_ms = utils::now<std::chrono::milliseconds>();
 
-  for ( u_int32_t tid = 0; tid < nthreads; ++tid )
+  for ( uint32_t tid = 0; tid < nthreads; ++tid )
   {
-    prod_X[tid] = new std::thread( [&](u_int32_t th_num ){
+    prod_X[tid] = new std::thread( [&](uint32_t th_num ){
 
       auto tp_start_ms = utils::now<std::chrono::milliseconds>();
 
-      u_int32_t conflicts = 0;
-      u_int32_t counter = 0;
+      uint32_t conflicts = 0;
+      uint32_t counter = 0;
       while (counter++ < single_thread_repeat)
       {
         auto tp_now = utils::now<std::chrono::nanoseconds>();
-        if ( mqueue.push(th_num, tp_now) == false)
+
+        // enqueue in a well defined queue_id
+        //if ( mqueue.push( th_num, tp_now) != core::result_t::eSuccess)
+        
+        // distributing load based on thread::id
+        if ( mqueue.push( tp_now) != core::result_t::eSuccess)
           ++conflicts;
       }
 
@@ -84,7 +89,7 @@ int main( int argc, const char* argv[] )
     }, tid );
   }
   
-  for ( u_int32_t tid = 0; tid < nthreads; ++tid )
+  for ( uint32_t tid = 0; tid < nthreads; ++tid )
   {
     prod_X[tid]->join();
   }
@@ -99,14 +104,16 @@ int main( int argc, const char* argv[] )
   //////////////////////////////////////////////
   // POP ALL ITEMS
   ////
-  u_int64_t  value = 0;
-  u_int32_t  pop_counter = 0;
+  uint64_t  value = 0;
+  uint32_t  pop_counter = 0;
 
   std::cout << "queue size=" << mqueue.size() << std::endl;
-  for ( u_int32_t read_counter = 0; read_counter < single_thread_repeat*nthreads; ++read_counter )
+  for ( uint32_t read_counter = 0; read_counter < single_thread_repeat*nthreads; ++read_counter )
   {
-    if ( mqueue.pop( value ) == false )
-      std::cout << "error pop" << std::endl;
+    if ( mqueue.pop( value ) != core::result_t::eSuccess )
+    {
+      //std::cout << "error pop" << std::endl;
+    }
 
     ++pop_counter;
   }
