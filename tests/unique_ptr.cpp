4bb5d25048590986c146445095deb7e1b2834da5
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-08-20 15:59:06 +0200

    add assertions

--- tests/unique_ptr.cpp
+++ tests/unique_ptr.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <assert.h>
 
 #include "core/mem_unique_ptr.h"
 
@@ -26,9 +27,11 @@ int main()
 {
   core::mem_unique_ptr<test_unique_ptr>  _ptr0 = new test_unique_ptr( "from raw pointer" );
   std::cout <<  "_ptr0.auto_delete() = " << _ptr0.auto_delete() << std::endl;
+  assert( _ptr0.get() != nullptr );
 
   core::mem_unique_ptr<test_unique_ptr>  _ptr1 = std::move(_ptr0);
   std::cout <<  "_ptr1.auto_delete() = " << _ptr1.auto_delete() << std::endl;
+  assert( _ptr1.get() != nullptr );
 
   std::cout <<  std::endl; 
   std::cout <<  std::endl; 
@@ -39,23 +42,29 @@ int main()
   std::cout <<  "_ptr2 = std::move(_ptr1);" << std::endl;
   std::cout <<  "_ptr1.get() = " << _ptr1.get() << " - _ptr1.auto_delete() = " << _ptr1.auto_delete() << std::endl;
   std::cout <<  "_ptr2.get() = " << _ptr2.get() << " - _ptr2.auto_delete() = " << _ptr2.auto_delete() << std::endl;
-
-  std::cout <<  std::endl; 
-  std::cout <<  std::endl; 
-  core::mem_unique_ptr<test_unique_ptr>  _ptr4 = std::make_unique<test_unique_ptr>( "from std::unique_ptr pointer" );
-  std::cout <<  "_ptr4.get() = " << _ptr4.get() << " - _ptr4.auto_delete() = " << _ptr4.auto_delete() << std::endl;
-  std::cout <<  std::endl; 
-  std::cout <<  std::endl; 
-  std::unique_ptr<test_unique_ptr>       _ptr5 = std::make_unique<test_unique_ptr>( "from std::unique_ptr pointer for assignment" );
-  core::mem_unique_ptr<test_unique_ptr>  _ptr6;
-  std::cout <<  "_ptr5.get() = " << _ptr5.get() << std::endl;
-  std::cout <<  "_ptr6.get() = " << _ptr6.get() << " - _ptr6.auto_delete() = " << _ptr6.auto_delete() << std::endl;
-  _ptr6 = std::move(_ptr5);
-  std::cout <<  "_ptr6 = std::move(_ptr5);" << std::endl;
-  std::cout <<  "_ptr5.get() = " << _ptr5.get() << std::endl;
-  std::cout <<  "_ptr6.get() = " << _ptr6.get() << " - _ptr6.auto_delete() = " << _ptr6.auto_delete() << std::endl;
+  assert( _ptr1.get() == nullptr );
+  assert( _ptr2.get() != nullptr );
 
 
+  std::cout <<  std::endl; 
+  std::cout <<  std::endl; 
+  core::mem_unique_ptr<test_unique_ptr>  _ptr3 = std::make_unique<test_unique_ptr>( "from std::unique_ptr pointer" );
+  std::cout <<  "_ptr3.get() = " << _ptr3.get() << " - _ptr3.auto_delete() = " << _ptr3.auto_delete() << std::endl;
+  assert( _ptr3.get() != nullptr );
+
+
+  std::cout <<  std::endl; 
+  std::cout <<  std::endl; 
+  std::unique_ptr<test_unique_ptr>       _ptr4 = std::make_unique<test_unique_ptr>( "from std::unique_ptr pointer for assignment" );
+  core::mem_unique_ptr<test_unique_ptr>  _ptr5;
+  std::cout <<  "_ptr4.get() = " << _ptr4.get() << std::endl;
+  std::cout <<  "_ptr5.get() = " << _ptr5.get() << " - _ptr5.auto_delete() = " << _ptr5.auto_delete() << std::endl;
+  _ptr5 = std::move(_ptr4);
+  std::cout <<  "_ptr5 = std::move(_ptr4);" << std::endl;
+  std::cout <<  "_ptr4.get() = " << _ptr4.get() << std::endl;
+  std::cout <<  "_ptr5.get() = " << _ptr5.get() << " - _ptr5.auto_delete() = " << _ptr5.auto_delete() << std::endl;
+  assert( _ptr4.get() == nullptr );
+  assert( _ptr5.get() != nullptr );
 
   return 0;
 }
