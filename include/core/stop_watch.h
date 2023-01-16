6b930af8bad6ab364fe35896f301d2454aea4cfb
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2023-01-16 23:58:47 +0100

    add stop_watch implementation

---
+++ include/core/stop_watch.h
@@ -0,0 +1,69 @@
+/**************************************************************************************************
+ * 
+ * Copyright 2022 https://github.com/fe-dagostino
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *************************************************************************************************/
+
+#ifndef CORE_STOP_WATCH_H
+#define CORE_STOP_WATCH_H
+
+#include <chrono>
+
+#include "core/types.h"
+#include "core/utils.h"
+
+namespace core {
+
+inline namespace LIB_VERSION {
+
+template<typename T, bool init = false>
+  requires    std::is_same_v<T, std::chrono::nanoseconds>  || std::is_same_v<T, std::chrono::microseconds> 
+           || std::is_same_v<T, std::chrono::milliseconds> || std::is_same_v<T, std::chrono::seconds> 
+           || std::is_same_v<T, std::chrono::minutes>      || std::is_same_v<T, std::chrono::hours> 
+           || std::is_same_v<T, std::chrono::days>         || std::is_same_v<T, std::chrono::weeks> 
+           || std::is_same_v<T, std::chrono::years>        || std::is_same_v<T, std::chrono::months> 
+class stop_watch final
+{
+public:  
+  /***/
+  constexpr stop_watch() noexcept
+  { if constexpr (init) { reset(); } }
+  /***/
+  constexpr stop_watch( const stop_watch& sw ) noexcept
+    : m_tp( sw.m_tp )
+  {}
+  /***/
+  constexpr ~stop_watch() noexcept
+  {}
+  /***/
+  constexpr void reset() noexcept
+  { m_tp = core::utils::now<T>(); }
+  /***/
+  constexpr auto peek() const noexcept
+  { return (core::utils::now<T>()-m_tp); }
+private:
+  T::rep   m_tp;
+};
+
+} // namespace LIB_VERSION 
+
+}
+
+#endif // CORE_STOP_WATCH_H
